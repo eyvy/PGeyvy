befd6035bdc36717f47cd6fb8204977dfe759c95
Clang <59293345+eyvy@users.noreply.github.com>
2019-12-31 07:28:12 +0330

    Create Why.c

---
+++ Why.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <windows.h>
+void gotoxy(int x, int y) //whooosh first function
+{
+    COORD coordinate;//allegedly  a structure of some sort
+    coordinate.X = x;//column
+    coordinate.Y = y;//row
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coordinate);//hande?:/
+}
+int main()
+{
+   gotoxy(7,7);
+   printf("yay");
+}
+
